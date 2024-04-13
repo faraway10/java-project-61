723c4da9ea2854be41b1538f62ae62fc65ddeb7b
faraway10 <42tree42@gmail.com>
2024-04-13 06:57:45 +0300

    Progression class refactored

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -16,22 +16,27 @@ public class Progression {
             int initNum = random.nextInt(100);
             int step = random.nextInt(8) + 2;
             int questNumIndex = random.nextInt(len);
-            StringJoiner joiner = new StringJoiner(" ");
 
-            for (var n = 0; n < len; n++) {
-                String currentNum = String.valueOf(initNum + n * step);
-                if (n != questNumIndex) {
-                    joiner.add(currentNum);
-                } else {
-                    joiner.add("..");
-                    answers[i] = currentNum;
-                }
-            }
-
-            questions[i] = joiner.toString();
+            questions[i] = getProgressionString(len, initNum, step, questNumIndex);
+            answers[i] = String.valueOf(initNum + questNumIndex * step);
         }
 
         String inviteText = "What number is missing in the progression?";
         Engine.runGame(inviteText, questions, answers);
+    }
+
+    public static String getProgressionString(int len, int initNum, int step, int questNumIndex) {
+        StringJoiner joiner = new StringJoiner(" ");
+
+        for (var n = 0; n < len; n++) {
+            String currentNum = String.valueOf(initNum + n * step);
+            if (n != questNumIndex) {
+                joiner.add(currentNum);
+            } else {
+                joiner.add("..");
+            }
+        }
+
+        return joiner.toString();
     }
 }
