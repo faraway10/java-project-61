04bc25c23709a958b245cc7558e73220e1a91fc0
faraway10 <42tree42@gmail.com>
2024-04-17 01:21:44 +0300

    .gitignore moved to the root directory; Makefile updated with build and test commands; plugin "java" removed from build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,4 @@
 plugins {
-    id("java")
     application
     checkstyle
 }
@@ -16,7 +15,7 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.1"))
+    testImplementation(platform("org.junit:junit-bom:5.10.2"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
